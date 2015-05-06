1843c61572c515c673ac2c61baf0ee5d0e0d26f8
Devendra Kavthekar <dek@odoo.com>
2015-05-06 12:38:25 +0530

    [Add] EditGroup ,Design Changes

--- groupme/controllers/controllers.py
+++ groupme/controllers/controllers.py
@@ -144,7 +144,8 @@ class GroupMe(http.Controller):
         except IntegrityError:
             return {'error': 'Integrity Error, Code must be unique!'}
         except Exception as e:
-            return {'error': 'Internal server error, please try again later or contact administrator.\nHere is the error message: %s' % e.message}
+            return {'error': 'Internal server error, please try again later or\
+                    contact administrator.\nHere is the error message: % s' % e.message}
         return {'url': "/networks/network/%s" % (network_id.id)}
 
     @http.route(['/networks/network/invite_people'], type='json', auth='user', methods=['POST'], website=True)
--- groupme/static/src/js/groupme_network.js
+++ groupme/static/src/js/groupme_network.js
@@ -1,29 +1,33 @@
-odoo.define('groupme.network', function (require) {
-	"use strict";
+odoo.define('groupme.network', function(require) {
+      "use strict";
       var ajax = require('web.ajax');
-	var time = require('web.time');
+      var time = require('web.time');
 
 
       var page_widgets = {};
       var characters = 200;
 
-	$(document).ready(function () {
-		$("timeago.timeago").each(function (index, el) {
-			var datetime = $(el).attr('datetime'),
-			datetime_obj = time.str_to_datetime(datetime),
-                  // if presentation 7 days, 24 hours, 60 min, 60 second, 1000 millis old(one week)
-                  // then return fix formate string else timeago
-                  display_str = "";
+      $(document).ready(function() {
+            $("#oe_main_menu_navbar").removeClass('navbar');
+            $("#oe_main_menu_navbar").removeClass('navbar-inverse');
+            $("#oe_main_menu_navbar").css('display', 'none');
+
+            $("timeago.timeago").each(function(index, el) {
+                  var datetime = $(el).attr('datetime'),
+                        datetime_obj = time.str_to_datetime(datetime),
+                        // if presentation 7 days, 24 hours, 60 min, 60 second, 1000 millis old(one week)
+                        // then return fix formate string else timeago
+                        display_str = "";
                   if (datetime_obj && new Date().getTime() - datetime_obj.getTime() > 7 * 24 * 60 * 60 * 1000) {
-                  	display_str = datetime_obj.toDateString();
+                        display_str = datetime_obj.toDateString();
                   } else {
-                  	display_str = $.timeago(datetime_obj);
+                        display_str = $.timeago(datetime_obj);
                   }
                   $(el).text(display_str);
             });
 
 
-            $("#trueMsgLabel p a, #falseMsgLabel p a").click(function(ev){
+            $("#trueMsgLabel p a, #falseMsgLabel p a").click(function(ev) {
                   ev.preventDefault();
                   var active_msg = $(this).data('active_msg');
                   var network_id = $(this).data('network_id');
@@ -31,20 +35,19 @@ odoo.define('groupme.network', function (require) {
                         'network_id': network_id,
                         'active': !(active_msg),
                   };
-                  ajax.jsonRpc("/networks/network/active_msg", 'call', values).then(function (data) {
+                  ajax.jsonRpc("/networks/network/active_msg", 'call', values).then(function(data) {
                         if (data.error) {
                               // TODO: Error Msg
                               console.log("ERROR : ", data.error);
-                        } else {                            
-                              if(data.result == true)
-                              {
+                        } else {
+                              if (data.result == true) {
                                     $('#trueMsgLabel, #falseMsgLabel').toggle();
                               }
                         }
                   });
             });
 
-            $("div .js_publish_management").click(function (ev) {                  
+            $("div .js_publish_management").click(function(ev) {
                   if ($(this).hasClass('css_published'))
                         $('#comment').hide();
                   else
@@ -52,20 +55,20 @@ odoo.define('groupme.network', function (require) {
             });
 
             // Allowed only (characters) in text-area
-            $("#txtMsg").keyup(function(){
-                if($(this).val().length > characters){
-                    $(this).val($(this).val().substr(0, characters));
+            $("#txtMsg").keyup(function() {
+                  if ($(this).val().length > characters) {
+                        $(this).val($(this).val().substr(0, characters));
                   }
                   var remaining = characters - $(this).val().length;
-                  $("#counter").html("You have <strong>"+remaining+"</strong> characters remaining");
-                  if(remaining <= 10)
-                      $("#counter").css("color","red");
+                  $("#counter").html("You have <strong>" + remaining + "</strong> characters remaining");
+                  if (remaining <= 10)
+                        $("#counter").css("color", "red");
                   else
-                      $("#counter").css("color","inherit");
+                        $("#counter").css("color", "inherit");
             });
-	});
-      
+      });
+
       return {
-          page_widgets: page_widgets,
+            page_widgets: page_widgets,
       };
 });
